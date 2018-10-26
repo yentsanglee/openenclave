9619db81a0e3aa0e23f151ab9f0522b882bdfd0e
Mike Brasher <mikbras@microsoft.com>
2018-10-26 14:16:16 -0500

    Clean up example.

--- tests/oefs/enc/enc.c
+++ tests/oefs/enc/enc.c
@@ -35,6 +35,23 @@ static void dump_dir(oefs_t* oefs, const char* dirname)
     oefs_closedir(dir);
 }
 
+static void create_files(oefs_t* oefs)
+{
+    const size_t NUM_FILES = 100;
+
+    for (size_t i = 0; i < NUM_FILES; i++)
+    {
+        char name[OEFS_PATH_MAX];
+        snprintf(name, sizeof(name), "filename-%zu", i);
+
+        oefs_file_t* file = NULL;
+        oefs_result_t r = __oefs_create_file(oefs, OEFS_ROOT_INO, name, &file);
+        OE_TEST(r == OEFS_OK);
+        OE_TEST(file != NULL);
+        oefs_close_file(file);
+    }
+}
+
 int test_oefs(const char* oefs_filename)
 {
     oe_block_dev_t* dev;
@@ -42,56 +59,30 @@ int test_oefs(const char* oefs_filename)
     oefs_t* oefs;
     oe_result_t result;
     oefs_result_t r;
-    const size_t NUM_FILES = 100;
     size_t size;
 
-    (void)NUM_FILES;
     (void)oefs;
 
-    {
-        r = oefs_compute_size(num_blocks, &size);
-        OE_TEST(r == OEFS_OK);
-    }
+    /* Compute the size of the OEFS file. */
+    r = oefs_compute_size(num_blocks, &size);
+    OE_TEST(r == OEFS_OK);
 
-#if 1
-    {
-        result = oe_open_host_block_dev("/tmp/oefs", &dev);
-        OE_TEST(result == OE_OK);
-    }
-#else
-    {
-        result = oe_open_ram_block_dev(size, &dev);
-        OE_TEST(result == OE_OK);
-        OE_TEST(dev != NULL);
-    }
-#endif
+    /* Ask the host to open the OEFS file. */
+    result = oe_open_host_block_dev(oefs_filename, &dev);
+    OE_TEST(result == OE_OK);
 
+    /* Initialize the OEFS file. */
+    r = oefs_initialize(dev, num_blocks);
+    OE_TEST(r == OEFS_OK);
 
-#if 1
-#ifdef INIT
-    {
-        r = oefs_initialize(dev, num_blocks);
-        OE_TEST(r == OEFS_OK);
-    }
-#endif
-#endif
-
+    /* Create an new OEFS object. */
     r = oefs_new(&oefs, dev);
     OE_TEST(r == OEFS_OK);
 
-    /* Create multiple files. */
-    for (size_t i = 0; i < NUM_FILES; i++)
-    {
-        char name[OEFS_PATH_MAX];
-        snprintf(name, sizeof(name), "filename-%zu", i);
+    /* Create some files. */
+    create_files(oefs);
 
-        oefs_file_t* file = NULL;
-        r = __oefs_create_file(oefs, OEFS_ROOT_INO, name, &file);
-        OE_TEST(r == OEFS_OK);
-        OE_TEST(file != NULL);
-        oefs_close_file(file);
-    }
-
+    /* Dump the root directory. */
     dump_dir(oefs, "/");
 
     oefs_delete(oefs);
