5bfd53e932ba88999afd8636f5430ef3a1eea8a1
Mike Brasher <mikbras@microsoft.com>
2018-11-02 11:23:36 -0500

    add hostfs.h (hostfs skeleton)

---
+++ libc/fs/hostfs.h
@@ -0,0 +1,11 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#ifndef _OE_HOSTFS_H
+#define _OE_HOSTFS_H
+
+#include "fs.h"
+
+fs_errno_t hostfs_initialize(fs_t** fs);
+
+#endif /* _OE_HOSTFS_H */
