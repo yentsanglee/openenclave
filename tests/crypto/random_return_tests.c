9629388723a2aaae1c0f60b029f8640eb962c0f5
vishnumohan1991 <vishnu.mohan@vvdntech.in>
2018-11-13 14:47:58 +0530

    Changes in rand test implementation

--- tests/crypto/random_return_tests.c
+++ tests/crypto/random_return_tests.c
@@ -1,16 +1,25 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 #define MAX_LOOP_SIZE 100000000
 #include <openenclave/internal/tests.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include "../../common/common.h"
+
 void TestRandreturn()
 {
     uint64_t rand_num;
+    printf("=== begin %s()\n", __FUNCTION__);
     for (uint64_t i = 0; i < MAX_LOOP_SIZE; i++)
     {
         rand_num = _rdrand();
-
-        OE_TEST(rand_num != 0);
+        if (rand_num == 0)
+        {
+            rand_num = _rdrand();
+            OE_TEST(rand_num != 0);
+        }
     }
+    printf("=== passed %s()\n", __FUNCTION__);
 }
