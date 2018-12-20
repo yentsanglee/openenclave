a635f626441c46517a21ef29f09f2f34eab2ba5f
Mike Brasher <mikbras@microsoft.com>
2018-12-19 19:10:21 -0600

    oefs: optimizations

--- fs/oefs/enclave/common.h
+++ fs/oefs/enclave/common.h
@@ -18,7 +18,7 @@ typedef struct _oefs_blk
 {
     union
     {
-        uint64_t words[OEFS_BLOCK_SIZE / sizeof(uint64_t)];
+        uint64_t u64[OEFS_BLOCK_SIZE / sizeof(uint64_t)];
         uint8_t data[OEFS_BLOCK_SIZE];
     }
     u;
@@ -27,19 +27,19 @@ typedef struct _oefs_blk
 OE_INLINE void oefs_blk_copy(oefs_blk_t* dest, const oefs_blk_t* src)
 {
     for (size_t i = 0; i < OEFS_BLOCK_SIZE / sizeof(uint64_t); i++)
-        dest->u.words[i] = src->u.words[i];
+        dest->u.u64[i] = src->u.u64[i];
 }
 
 OE_INLINE void oefs_blk_clear(oefs_blk_t* blk)
 {
     for (size_t i = 0; i < OEFS_BLOCK_SIZE / sizeof(uint64_t); i++)
-        blk->u.words[i] = 0;
+        blk->u.u64[i] = 0;
 }
 
 OE_INLINE bool oefs_blk_equal(const oefs_blk_t* b1, const oefs_blk_t* b2)
 {
-    const uint64_t* p = b1->u.words;
-    const uint64_t* q = b2->u.words;
+    const uint64_t* p = b1->u.u64;
+    const uint64_t* q = b2->u.u64;
     size_t n = OEFS_BLOCK_SIZE / sizeof(uint64_t);
 
     while (n && *p == *q)
