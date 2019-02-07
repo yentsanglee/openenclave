90ce319662c9a2f13d4224164afc6d990e484e3f
Mike Brasher <mikbras@microsoft.com>
2019-02-07 13:23:45 -0600

    add common.h for all corelibc files.

---
+++ include/openenclave/corelibc/bits/common.h
@@ -0,0 +1,7 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#if defined(OE_NEED_STDC_NAMES) && !defined(OE_NEED_STDC_DEFINES)
+#define OE_NEED_STDC_DEFINES
+#define OE_DEFINED_STDC_DEFINES
+#endif
