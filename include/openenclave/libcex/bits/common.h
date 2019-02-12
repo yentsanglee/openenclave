9f8cf447af98203e823380c81f15ba17f1cb0df6
Mike Brasher <mikbras@microsoft.com>
2019-02-12 13:40:25 -0600

    new libcex/bits/common.h

---
+++ include/openenclave/libcex/bits/common.h
@@ -0,0 +1,17 @@
+/* Copyright (c) Microsoft Corporation. All rights reserved. */
+/* Licensed under the MIT License. */
+
+#ifndef _LIBCEX_COMMON_H
+#define _LIBCEX_COMMON_H
+
+#define OE_FILE_INSECURE OE_DEVICE_ID_HOSTFS
+#define OE_FILE_SECURE_HARDWARE OE_DEVICE_ID_SHWFS
+#define OE_FILE_SECURE_ENCRYPTION OE_DEVICE_ID_SGXFS
+
+#ifdef OE_USE_OPTEE
+#define OE_FILE_SECURE_BEST_EFFORT OE_FILE_SECURE_HARDWARE
+#else
+#define OE_FILE_SECURE_BEST_EFFORT OE_FILE_SECURE_ENCRYPTION
+#endif
+
+#endif /* _LIBCEX_COMMON_H */
