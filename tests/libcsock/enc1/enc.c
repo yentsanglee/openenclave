f20eb13f00ddb8e86c8c23adf0df3784e7a0d8f0
Mike Brasher <mikbras@microsoft.com>
2019-03-01 19:31:57 -0600

    test libssock with corelibc

--- tests/libcsock/enc1/enc.c
+++ tests/libcsock/enc1/enc.c
@@ -2,32 +2,19 @@
 // Licensed under the MIT License.
 
 #include <openenclave/corelibc/sys/socket.h>
-#include <openenclave/corelibc/sys/utsname.h>
 #include <openenclave/enclave.h>
-#include <openenclave/internal/device.h>
-#include <openenclave/internal/tests.h>
-#include <pthread.h>
-#include <stdio.h>
-#include <unistd.h>
 #include "../client.h"
 #include "../server.h"
 
-static pthread_once_t once;
-
-void _initialize(void)
-{
-    oe_set_default_socket_devid(OE_DEVID_HOST_SOCKET);
-}
-
 void run_enclave_server(uint16_t port)
 {
-    pthread_once(&once, _initialize);
+    oe_set_default_socket_devid(OE_DEVID_HOST_SOCKET);
     run_server(port);
 }
 
 void run_enclave_client(uint16_t port)
 {
-    pthread_once(&once, _initialize);
+    oe_set_default_socket_devid(OE_DEVID_HOST_SOCKET);
     run_client(port);
 }
 
