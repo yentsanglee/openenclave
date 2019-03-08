6a8f7d87af009da980c2f758230c7103106f33b1
Mike Brasher <mikbras@microsoft.com>
2019-03-07 18:17:03 -0600

    Complete exec(hello world)

--- tests/exec/main/main.c
+++ tests/exec/main/main.c
@@ -2,17 +2,10 @@
 // Licensed under the MIT License.
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, const char* argv[])
 {
-#if 0
-    for (int i = 0; i < argc; i++)
-    {
-        printf("argv[%d]=%s\n", i, argv[i]);
-    }
-#endif
-
     printf("Hello world!\n");
-
-    return 0;
+    exit(1);
 }
