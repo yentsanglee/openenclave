6d1f28feacae9446edcace81248a94f902d564e2
Mike Brasher <mikbras@microsoft.com>
2019-06-09 15:11:40 -0500

    new samples

--- libc/stubs.c
+++ libc/stubs.c
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 #include <assert.h>
 #include <stdlib.h>
 #include <unistd.h>
--- tools/oewrap/sample/ssl/main.c
+++ tools/oewrap/sample/ssl/main.c
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 #include <openssl/crypto.h>
 #include <openssl/err.h>
 #include <openssl/pem.h>
