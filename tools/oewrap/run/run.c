0d2e8473c16923f2b0c7480bfc6fefdbe721e576
Mike Brasher <mikbras@microsoft.com>
2019-06-08 20:00:37 -0500

    add run app.

---
+++ tools/oewrap/run/run.c
@@ -0,0 +1,145 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#include <limits.h>
+#include <openenclave/host.h>
+#include <stdio.h>
+#include "wrap_u.h"
+
+static int _serialize_argv(
+    int argc,
+    const char* argv[],
+    void** argv_buf,
+    size_t* argv_buf_size)
+{
+    int ret = -1;
+    char** new_argv = NULL;
+    size_t array_size;
+    size_t alloc_size;
+
+    if (argv_buf)
+        *argv_buf = NULL;
+
+    if (argv_buf_size)
+        *argv_buf_size = 0;
+
+    if (argc < 0 || !argv || !argv_buf || !argv_buf_size)
+        goto done;
+
+    /* Calculate the total allocation size. */
+    {
+        /* Calculate the size of the null-terminated array of pointers. */
+        array_size = ((size_t)argc + 1) * sizeof(char*);
+
+        alloc_size = array_size;
+
+        /* Calculate the sizeo of the individual zero-terminated strings. */
+        for (int i = 0; i < argc; i++)
+            alloc_size += strlen(argv[i]) + 1;
+    }
+
+    /* Allocate space for the array followed by strings. */
+    if (!(new_argv = (char**)malloc(alloc_size)))
+        goto done;
+
+    /* Copy each string. */
+    {
+        char* p = (char*)&new_argv[argc + 1];
+        size_t offset = array_size;
+
+        for (int i = 0; i < argc; i++)
+        {
+            size_t size = strlen(argv[i]) + 1;
+
+            memcpy(p, argv[i], size);
+            new_argv[i] = (char*)offset;
+            p += size;
+            offset += size;
+        }
+
+        new_argv[argc] = NULL;
+    }
+
+    *argv_buf = new_argv;
+    *argv_buf_size = alloc_size;
+    new_argv = NULL;
+
+    ret = 0;
+
+done:
+
+    if (new_argv)
+        free(new_argv);
+
+    return ret;
+}
+
+int main(int argc, const char* argv[])
+{
+    const char* arg0 = argv[0];
+    oe_result_t r;
+    oe_enclave_t* enc = NULL;
+    const uint32_t flags = OE_ENCLAVE_FLAG_DEBUG;
+    const oe_enclave_type_t type = OE_ENCLAVE_TYPE_SGX;
+    int retval;
+    void* argv_buf = NULL;
+    size_t argv_buf_size = 0;
+    const char* path = NULL;
+
+    if (argc < 2)
+    {
+        fprintf(stderr, "Usage: %s enclave args...\n", argv[0]);
+        exit(1);
+    }
+
+    /* Create the enclave. */
+    {
+        r = oe_create_wrap_enclave(argv[1], type, flags, NULL, 0, &enc);
+
+        if (r != OE_OK)
+        {
+            fprintf(stderr, "%s: oe_create_test_hostfs_enclave()\n", argv[0]);
+            exit(1);
+        }
+    }
+
+    /* Remove the first argument. */
+    argc--;
+    argv++;
+
+    /* Serialize the arguments. */
+    if (_serialize_argv(argc, argv, &argv_buf, &argv_buf_size) != 0)
+    {
+        fprintf(stderr, "%s: _serialize_argv()\n", arg0);
+        exit(1);
+    }
+
+    /* Run the enclave. */
+    {
+        r = oe_wrap_main_ecall(enc, &retval, argc, argv_buf, argv_buf_size);
+
+        if (r != OE_OK)
+        {
+            fprintf(stderr, "%s: oe_wrap_main_ecall()\n", arg0);
+            exit(1);
+        }
+    }
+
+    /* Terminate the enclave. */
+    {
+        r = oe_terminate_enclave(enc);
+
+        if (r != OE_OK)
+        {
+            fprintf(stderr, "%s: oe_terminate_enclave()\n", arg0);
+            exit(1);
+        }
+    }
+
+    /* Remove the file. */
+    remove(path);
+
+    free(argv_buf);
+
+    return retval;
+}
