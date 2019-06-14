e50edd3c3736851323bcb746306823d0d3fb6436
Mike Brasher <mikbras@microsoft.com>
2019-06-14 11:25:54 -0500

    extend mbed testing

--- tests/mbed/enc/selftest_wrapper.c
+++ tests/mbed/enc/selftest_wrapper.c
@@ -4,4 +4,7 @@
 #include <openenclave/corelibc/stdio.h>
 #include <openenclave/corelibc/stdlib.h>
 
+#define EXIT_SUCCESS 0
+#define EXIT_FAILURE 1
+
 #include "../../../3rdparty/mbedtls/mbedtls/programs/test/selftest.c"
