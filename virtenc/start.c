5756da00fb744a5f50e566173fef61f7e1f8a50e
Mike Brasher <mikbras@microsoft.com>
2019-06-14 21:12:31 -0500

    added constructors/destructor functions.

--- virtenc/start.c
+++ virtenc/start.c
@@ -1,9 +1,20 @@
 #include <freestanding/print.h>
 #include <freestanding/exit.h>
 
+void fs_call_init_functions(void);
+void fs_call_fini_functions(void);
+
+__attribute__((constructor)) void _constructor(void)
+{
+    fs_print_str("_constructor()\n");
+}
+
 void _start()
 {
+    fs_call_init_functions();
+
     fs_print_str("_start()\n");
+
     fs_print_oct(0777);
     fs_print_nl();
     fs_print_uint(99);
@@ -13,5 +24,6 @@ void _start()
     fs_print_hex(0xABCD);
     fs_print_nl();
 
+    fs_call_fini_functions();
     fs_exit(99);
 }
