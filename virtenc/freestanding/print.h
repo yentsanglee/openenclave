e01ddb86d0fe03f4080505b58d8669c97a8d6aa6
Mike Brasher <mikbras@microsoft.com>
2019-06-14 21:03:31 -0500

    put -> print

--- virtenc/freestanding/print.h
+++ virtenc/freestanding/print.h
@@ -9,39 +9,39 @@
 #include "../freestanding/syscall.h"
 #include "../freestanding/string.h"
 
-FS_INLINE void fs_put_str(const char* s)
+FS_INLINE void fs_print_str(const char* s)
 {
     fs_syscall3(FS_SYS_write, 1, (long)s, fs_strlen(s));
 }
 
-FS_INLINE void fs_put_nl(void)
+FS_INLINE void fs_print_nl(void)
 {
     const char nl = '\n';
     fs_syscall3(FS_SYS_write, 1, (long)&nl, 1);
 }
 
-FS_INLINE void fs_put_oct(uint64_t x)
+FS_INLINE void fs_print_oct(uint64_t x)
 {
     fs_intstr_buf_t buf;
-    fs_put_str(fs_uint64_octstr(&buf, x, NULL));
+    fs_print_str(fs_uint64_octstr(&buf, x, NULL));
 }
 
-FS_INLINE void fs_put_uint(uint64_t x)
+FS_INLINE void fs_print_uint(uint64_t x)
 {
     fs_intstr_buf_t buf;
-    fs_put_str(fs_uint64_decstr(&buf, x, NULL));
+    fs_print_str(fs_uint64_decstr(&buf, x, NULL));
 }
 
-FS_INLINE void fs_put_int(int64_t x)
+FS_INLINE void fs_print_int(int64_t x)
 {
     fs_intstr_buf_t buf;
-    fs_put_str(fs_int64_decstr(&buf, x, NULL));
+    fs_print_str(fs_int64_decstr(&buf, x, NULL));
 }
 
-FS_INLINE void fs_put_hex(uint64_t x)
+FS_INLINE void fs_print_hex(uint64_t x)
 {
     fs_intstr_buf_t buf;
-    fs_put_str(fs_uint64_hexstr(&buf, x, NULL));
+    fs_print_str(fs_uint64_hexstr(&buf, x, NULL));
 }
 
 #endif /* _FREESTANDING_PRINT_H */
--- virtenc/start.c
+++ virtenc/start.c
@@ -3,15 +3,15 @@
 
 void _start()
 {
-    fs_put_str("_start()\n");
-    fs_put_oct(0777);
-    fs_put_nl();
-    fs_put_uint(99);
-    fs_put_nl();
-    fs_put_int(-123);
-    fs_put_nl();
-    fs_put_hex(0xABCD);
-    fs_put_nl();
+    fs_print_str("_start()\n");
+    fs_print_oct(0777);
+    fs_print_nl();
+    fs_print_uint(99);
+    fs_print_nl();
+    fs_print_int(-123);
+    fs_print_nl();
+    fs_print_hex(0xABCD);
+    fs_print_nl();
 
     fs_exit(99);
 }
