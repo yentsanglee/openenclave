1e7db95ef968c6ebc2003d097cac4a29f7a6d6c0
Mike Brasher <mikbras@microsoft.com>
2019-06-28 15:29:16 -0500

    ve: added vsample

--- venclave/enclave/test_t_wrap.c
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
-#include <string.h>
-
-#define memset ve_memset
-#define memcpy ve_memcpy
-
-#include "test_t.c"
