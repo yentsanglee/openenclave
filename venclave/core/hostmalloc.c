135089b7dc7be6f01838215d16918cd660c240d3
Mike Brasher <mikbras@microsoft.com>
2019-07-09 09:12:37 -0500

    add hostmalloc.c file

---
+++ venclave/core/hostmalloc.c
@@ -0,0 +1,77 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#include <openenclave/enclave.h>
+#include "calls.h"
+#include "globals.h"
+#include "panic.h"
+#include "string.h"
+
+void* oe_host_malloc(size_t size)
+{
+    uint64_t retval = 0;
+    const int sock = __ve_thread_sock_tls;
+
+    if (ve_call1(sock, VE_FUNC_MALLOC, &retval, size) != 0)
+        return NULL;
+
+    return (void*)retval;
+}
+
+void* oe_host_calloc(size_t nmemb, size_t size)
+{
+    uint64_t retval = 0;
+    const int sock = __ve_thread_sock_tls;
+
+    if (ve_call2(sock, VE_FUNC_CALLOC, &retval, nmemb, size) != 0)
+        return NULL;
+
+    return (void*)retval;
+}
+
+void* oe_host_realloc(void* ptr, size_t size)
+{
+    uint64_t retval = 0;
+    const int sock = __ve_thread_sock_tls;
+
+    if (ve_call2(sock, VE_FUNC_REALLOC, &retval, (uint64_t)ptr, size) != 0)
+        return NULL;
+
+    return (void*)retval;
+}
+
+void oe_host_free(void* ptr)
+{
+    const int sock = __ve_thread_sock_tls;
+
+    if (ve_call1(sock, VE_FUNC_FREE, NULL, (uint64_t)ptr) != 0)
+        ve_panic("oe_host_free() failed");
+}
+
+char* oe_host_strndup(const char* str, size_t n)
+{
+    char* p;
+    size_t len;
+
+    if (!str)
+        return NULL;
+
+    len = ve_strlen(str);
+
+    if (n < len)
+        len = n;
+
+    /* Would be an integer overflow in the next statement. */
+    if (len == OE_SIZE_MAX)
+        return NULL;
+
+    if (!(p = oe_host_malloc(len + 1)))
+        return NULL;
+
+    if (ve_memcpy(p, str, len) != OE_OK)
+        return NULL;
+
+    p[len] = '\0';
+
+    return p;
+}
